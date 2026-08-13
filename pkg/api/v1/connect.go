4ff50a1fe6a100bfabc43138cf834399d9d5caca
rhpidfyre <brandon@rhpidfyre.io>
2026-08-12 23:49:58 -0400

    Fix `/connect` end point returning a redundant "500 record not found" status

--- pkg/api/v1/connect.go
+++ pkg/api/v1/connect.go
@@ -2,10 +2,12 @@ package v1
 
 import (
 	"context"
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/unixtensor/monolith/pkg/datastore"
+	"gorm.io/gorm"
 )
 
 func add_game(v1 *V1, bg_ctx context.Context, gin_ctx *gin.Context, placeid, jobid string) error {
@@ -41,7 +43,7 @@ func (v1 *V1) connected(bg_ctx context.Context) gin.HandlerFunc {
 		placeid := gin_ctx.Param("placeId")
 
 		j, err := v1.DS.GetGame(bg_ctx, placeid)
-		if err != nil {
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
 			InternalError(gin_ctx, err)
 			return
 		}
