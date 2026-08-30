f98c8c12b3ae458b6dfccc5f6a2a443527a0d31c
rhpidfyre <brandon@rhpidfyre.io>
2026-08-30 07:47:24 -0400

    Web: studio icon and use new uptime timestamp

--- web/src/app/games/button/server.tsx
+++ web/src/app/games/button/server.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Link, type To } from "react-router";
 import type { JobsSerialized } from "@/app/dashboard/providers/jobs";
-import { ClockIcon, UsersIcon } from "lucide-react";
+import { ClockIcon, HammerIcon, UsersIcon } from "lucide-react";
 import { formatDistanceToNow } from "date-fns";
 
 function Data({
@@ -20,20 +20,32 @@ function Data({
 }
 
 function Metadata({ job }: { job: JobsSerialized }) {
-	const job_elapsed = new Date(Date.now() - job.Job.UpTime);
-
 	return (
 		<div className="flex gap-3 opacity-60 [&>div]:flex [&>div]:gap-1 [&>div]:items-center">
 			<Data icon={<UsersIcon />}>
 				{Object.entries(job.Job.Players).length}
 			</Data>
 			<Data icon={<ClockIcon />}>
-				{formatDistanceToNow(job_elapsed, {
+				{formatDistanceToNow(new Date(job.Job.UpTime), {
 					addSuffix: true,
 				})}
 			</Data>
 		</div>
 	);
+}
+
+function Name({ job }: { job: JobsSerialized }) {
+	const is_studio = job.Id.startsWith("studio");
+	const id = <h1 className="font-bold text-lg">{job.Id}</h1>;
+
+	if (is_studio)
+		return (
+			<div className="flex items-center gap-2">
+				<HammerIcon className="text-[#0c86d9] size-5" />
+				{id}
+			</div>
+		);
+	return id;
 }
 
 export function ServerButton({ to, job }: { to: To; job: JobsSerialized }) {
@@ -44,7 +56,7 @@ export function ServerButton({ to, job }: { to: To; job: JobsSerialized }) {
 					variant="outline"
 					className="flex flex-col justify-between items-baseline h-fit w-full p-5 [&>a]:min-w-full"
 				>
-					<h1 className="font-bold text-lg">{job.Id}</h1>
+					<Name job={job} />
 					<Metadata job={job} />
 				</Button>
 			</Link>
