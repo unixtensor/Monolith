bfae08dbb8a73a7c9b0b3b5ead34d7dba0ebd143
rhpidfyre <brandon@rhpidfyre.io>
2026-09-23 23:12:54 -0400

    Fix monaco re-rendering every page visit to `/sandbox`

--- web/src/pages/SandboxPage.tsx
+++ web/src/pages/SandboxPage.tsx
@@ -2,13 +2,25 @@ import { Card, CardHeader } from "@/components/ui/card";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useTitle } from "@/hooks/useTitle";
 import { registerLuau } from "@/lib/luau.monarch";
-import { Editor } from "@monaco-editor/react";
+import { useTheme } from "@/providers/ThemeProvider";
+import { Editor, type Monaco } from "@monaco-editor/react";
+import type { editor } from "monaco-editor";
+import { useRef } from "react";
 
 const default_luau_code: string = 'print("Hello World!")\n';
+const model_path: string = "inmemory://sandbox/main.luau";
+
+const monaco_registered = new WeakSet<Monaco>();
+
+function monaco_register_luau(monaco: Monaco) {
+	if (monaco_registered.has(monaco)) return;
+	monaco_registered.add(monaco);
+	registerLuau(monaco);
+}
 
 function Explorer() {
 	return (
-		<Card className="w-full gap-2 h-fit">
+		<Card className="w-150 gap-2 h-fit">
 			<CardHeader>
 				<Tabs defaultValue="explorer">
 					<TabsList className="w-full">
@@ -21,23 +33,50 @@ function Explorer() {
 	);
 }
 
+function useEditorTheme() {
+	const current_theme = useTheme();
+	const system_theme = function () {
+		return window.matchMedia("(prefers-color-scheme: dark)").matches
+			? "luau-dark"
+			: "luau-light";
+	};
+	return (
+		(current_theme.theme === "system" && system_theme()) ||
+		(current_theme.theme === "light" && "luau-light") ||
+		"luau-dark"
+	);
+}
+
 export default function SandboxPage() {
 	useTitle("Sandbox");
+	const editor_theme = useEditorTheme();
+	const editor_ref = useRef<editor.IStandaloneCodeEditor | null>(null);
 
 	return (
-		<div className="flex gap-5 h-full min-h-0 overflow-hidden">
-			<div className="size-full">
-				<Editor
-					theme="luau-dark"
-					value={default_luau_code}
-					beforeMount={registerLuau}
-					height="100%"
-					language="luau"
-					defaultLanguage="luau"
-					options={{ automaticLayout: true }}
-				/>
+		<div className="flex flex-col overflow-hidden gap-2 h-full">
+			{/*<Menu monaco={editor_ref} />*/}
+			<div className="flex gap-5 h-full">
+				<div className="size-full border rounded-xl p-1">
+					<Editor
+						theme={editor_theme}
+						defaultValue={default_luau_code}
+						path={model_path}
+						keepCurrentModel
+						beforeMount={monaco_register_luau}
+						onMount={(i) => {
+							editor_ref.current = i;
+						}}
+						height="100%"
+						language="luau"
+						defaultLanguage="luau"
+						options={{
+							automaticLayout: true,
+							fontSize: 16,
+						}}
+					/>
+				</div>
+				<Explorer />
 			</div>
-			<Explorer />
 		</div>
 	);
 }
